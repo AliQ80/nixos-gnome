333a79faef983d83bb0f0a44195b83570b964671
Ali Karam <ak@alikaram.com>
2024-10-16 16:50:49 +0300

    change helix theme

--- user/apps/tui/helix.nix
+++ user/apps/tui/helix.nix
@@ -1,10 +1,22 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     helix
   ];
+
   programs.helix = {
     enable = true;
+
+    settings = {
+      theme = "catppuccin-frappe-glass";
+    };
+
+    themes = {
+      catppuccin-frappe-glass = {
+        inherits = "catppuccin_frappe";
+        "ui.background" = "";
+      };
+    };
   };
 }
