43cfcb0a34a5b658373ea22af5312f22efe1582c
Ali Karam <ak@alikaram.com>
2025-03-05 10:12:09 +0300

    add zed editor and moved unfree software to apps.nix

--- user/apps/gui/apps.nix
+++ user/apps/gui/apps.nix
@@ -3,14 +3,22 @@
 {
   home.packages = with pkgs; [
     localsend
-    emacs29-pgtk
-    jetbrains.pycharm-community
+    # emacs29-pgtk
     meld
     python3
     obs-studio
     krita
     scribus
+    zed-editor
+    
+    # unfree
+    jetbrains.pycharm-community
     whatsapp-for-linux
+    obsidian
+    vivaldi
+    google-chrome
+    discord
+    zoom-us
   ];
 
   programs.vscode.enable = true;
